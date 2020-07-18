5279c7f94bed86d46d37ffdee413f90e3fc9682b
KWANGYOUNGCHOI <cky1203@gmail.com>
2020-07-18 19:32:47 +0900

    Add hike view, detail

---
+++ SwiftuiTutorial/Landmarks/HikeDetail.swift
@@ -0,0 +1,21 @@
+//
+//  HikeDetail.swift
+//  SwiftuiTutorial
+//
+//  Created by CHOI KWANGYOUNG on 2020/07/18.
+//  Copyright © 2020 CHOI KWANGYOUNG. All rights reserved.
+//
+
+import SwiftUI
+
+struct HikeDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HikeDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        HikeDetail()
+    }
+}
---
+++ SwiftuiTutorial/Landmarks/HikeView.swift
@@ -0,0 +1,21 @@
+//
+//  HikeView.swift
+//  SwiftuiTutorial
+//
+//  Created by CHOI KWANGYOUNG on 2020/07/18.
+//  Copyright © 2020 CHOI KWANGYOUNG. All rights reserved.
+//
+
+import SwiftUI
+
+struct HikeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HikeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HikeView()
+    }
+}
